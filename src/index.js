c31c62238032b1557301006bc854a6a34d70e771
SalmaAlMutawa <s.almutawa96@gmail.com>
2018-11-20 14:43:29 +0300

    cardItem styling done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//styles
+import "./index.css";
+import "./css/skel.css";
+import "./css/style-mobile.css";
+import "./css/style-narrow.css";
+import "./css/style-narrower.css";
+import "./css/style-normal.css";
+import "./css/style-wide.css";
+import "./css/style.css";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
