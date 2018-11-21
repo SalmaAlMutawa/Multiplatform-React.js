ebfc606979e2d137f7a1275d200c860d5279cde1
Mariam Ali <mariamqali13@gmail.com>
2018-11-21 15:12:32 +0300

    done with auth for now

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import TopNavBar from "./Navigation/TopNavBar";
+// import TopNavBar from "./Navigation/TopNavBar";
+import AuthButton from "./AuthButton";
 
 class Home extends Component {
   render() {
@@ -15,6 +16,8 @@ class Home extends Component {
             <Link to="/list">
               <button className="btn btn-info btn-lg">Start Shopping</button>
             </Link>
+
+            <AuthButton />
           </div>
         </div>
       </div>
