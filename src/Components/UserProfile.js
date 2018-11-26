597e5a0bc74d46af18dd47d99dc88b7c2b0d1c2d
SalmaAlMutawa <s.almutawa96@gmail.com>
2018-11-26 22:52:07 +0300

    authentication done for checkout page and some other changes in profile page

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,91 +1,82 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import SideBar from "./Navigation/SideBar";
 
 class UserProfile extends Component {
   render() {
     console.log(this.props.user);
     return (
-      <div
-        className="jumbotron"
-        style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-      >
-        <div className="container">
-          <form>
-            <div className="form-group row">
-              <label for="firstName" className="col-sm-2 col-form-label">
-                First Name
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={this.props.user.first_name}
-                  style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-                />
+      <div>
+        <SideBar />
+        <div
+          className="jumbotron"
+          style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+        >
+          <div className="container">
+            <form>
+              <div className="form-group row">
+                <label for="firstName" className="col-sm-2 col-form-label">
+                  First Name
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={this.props.user.first_name}
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <label for="lastName" className="col-sm-2 col-form-label">
-                Last Name
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={this.props.user.last_name}
-                  style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-                />
+              <div className="form-group row">
+                <label for="lastName" className="col-sm-2 col-form-label">
+                  Last Name
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={this.props.user.last_name}
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="form-group row">
-              <label for="username" className="col-sm-2 col-form-label">
-                Username
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={this.props.user.username}
-                  style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-                />
+              <div className="form-group row">
+                <label for="username" className="col-sm-2 col-form-label">
+                  Username
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={this.props.user.username}
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <label for="email" className="col-sm-2 col-form-label">
-                Email
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={this.props.user.email}
-                  style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-                />
+              <div className="form-group row">
+                <label for="email" className="col-sm-2 col-form-label">
+                  Email
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={this.props.user.email}
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+                  />
+                </div>
               </div>
+            </form>
+            <div className="container text-center">
+              <button type="button" class="btn btn-outline-warning btn-lg">
+                Change Password
+              </button>
+              <button type="button" class="btn btn-outline-info btn-lg mx-3">
+                Edit Profile
+              </button>
             </div>
-            <div className="form-group row">
-              <label for="shippingAddress" className="col-sm-2 col-form-label">
-                Shipping Address
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  value="this.props.user.shipping_address"
-                  style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-                />
-              </div>
-            </div>
-          </form>
-          <div className="container text-center">
-            <button type="button" class="btn btn-outline-warning btn-lg">
-              Change Password
-            </button>
-            <button type="button" class="btn btn-outline-info btn-lg mx-3">
-              Edit Profile
-            </button>
           </div>
         </div>
       </div>
