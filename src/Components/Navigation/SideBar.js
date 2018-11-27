ffd41c8ba253d848da871e8bacd387a70ac9340e
SalmaAlMutawa <s.almutawa96@gmail.com>
2018-11-27 22:22:00 +0300

    side-nav fixed

--- src/Components/Navigation/SideBar.js
+++ src/Components/Navigation/SideBar.js
@@ -2,30 +2,40 @@ import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actionCreators from "../../Store/actions";
+import "../../main.css";
 
 class SideBar extends Component {
   render() {
     return (
-      <div class="row">
-        <div class="col-2">
-          <nav
-            className="nav nav-dark"
-            style={{ backgroundColor: "rgba(0,0,0,0.75)" }}
-          >
-            <Link to="#" className="nav-link active" style={{ color: "white" }}>
-              Orders
-            </Link>
-            <Link to="#" className="nav-link active" style={{ color: "white" }}>
-              Saved Addresses
-            </Link>
-            <Link to="#" className="nav-link active" style={{ color: "white" }}>
-              Edit Profile
-            </Link>
-            <Link to="#" className="nav-link active" style={{ color: "white" }}>
-              About
-            </Link>
-          </nav>
-        </div>
+      <div className="sidenav">
+        <Link
+          to="#"
+          className="nav-link active"
+          style={{ color: "white", lineHeight: "3" }}
+        >
+          Orders
+        </Link>
+        <Link
+          to="#"
+          className="nav-link active"
+          style={{ color: "white", lineHeight: "1.5" }}
+        >
+          Saved Addresses
+        </Link>
+        <Link
+          to="#"
+          className="nav-link active"
+          style={{ color: "white", lineHeight: "3" }}
+        >
+          Edit Profile
+        </Link>
+        <Link
+          to="#"
+          className="nav-link active"
+          style={{ color: "white", lineHeight: "3" }}
+        >
+          About
+        </Link>
       </div>
     );
   }
